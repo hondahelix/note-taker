8732ae9dbfec7d0e29641ebd99de4c9ec0c76683
Jonathan Honda <hondahelix120@gmail.com>
2020-10-15 18:47:23 -0700

    gave each saved file a new id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,4 +4,11 @@ module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
         res.json(noteData);
       });
+    app.post("/api/notes", function(req, res) {
+        console.log(req.body.title);
+        if(req.body.title !=="" && req.body.text !==""){
+            noteData.push(req.body);
+            res.json(true);
+        }
+    });
 };
